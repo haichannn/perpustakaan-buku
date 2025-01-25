bdf409cf6a59b26febcdf4cefd21f2394d1e60f6
haichannn <muhammadhaichal304@gmail.com>
2025-01-25 12:30:23 +0700

    Merge branch refactor/auth/register to main branch

--- register.php
+++ register.php
@@ -7,6 +7,8 @@ require_once "models/users/registerUser.php";
 require_once "utils/alert.php";
 require_once "utils/hashPassword.php";
 
+$messagesErrors = null;
+
 // Jika tombol daftar ditekan 
 if (isset($_POST["tombol-daftar"])) {
     $sanitizeInput = InputSanitize($_POST);
@@ -31,14 +33,9 @@ if (isset($_POST["tombol-daftar"])) {
             // masukan data user ke database
             $resultRegisterUser = RegisterUser($usernameClean, $resultHashPassword);
 
-            // jika berhasil
-            if ($resultRegisterUser == 1) {
-                echo Alert("berhasil daftar :b, silakan login", true);
-            } else {
-                echo Alert("gagal daftar :(, silakan coba lagi", false);
-            }
+            $messagesErrors = ($resultRegisterUser !== 1) ? "gagal daftar :(, silakan coba lagi" : null;
         } else {
-            echo Alert("username sudah digunakan", false);
+            $messagesErrors = "Username tidak tersedia";
         }
     }
 }
@@ -56,53 +53,73 @@ if (isset($_POST["tombol-daftar"])) {
     <script src="assets/js/bootstrap.bundle.js"></script>
 </head>
 
-<body style="background-color: #008F74;">
+<body style="background-color: #508bfc;">
     <main>
         <?php
         include "layouts/navbar.php"
         ?>
 
-
         <!-- Login form start -->
-        <section class="vh-100" >
-            <section class="container py-5 h-100">
+        <section class="vh-100">
+            <section class="container py-5 h-100 ">
                 <div class="row d-flex justify-content-center align-items-center h-100">
-                    <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                        <div class="card shadow-2-strong" style="border-radius: 1rem;">
+                    <div class="col-12 col-md-8 col-lg-9">
+                        <div class="card shadow-2-strong" style="border-radius: .5rem;">
                             <div class="card-body p-4">
-                                <form method="post">
-
-                                    <div class="mb-5 text-center">
-                                        <h3>Sign up</h3>
-                                    </div>
-
-                                    <div class="mb-4">
-                                        <?php $hasErrorUsername = isset($resultValidation["username"]) ?>
-                                        <p class="text-danger"><i><?= $hasErrorUsername ? $resultValidation["username"] : null  ?></i></p>
-                                        <div class="col-sm-10 w-100">
-                                            <input type="text" class="form-control <?= $hasErrorUsername ? 'is-invalid' : null ?>" name="username" placeholder="username" />
+                                <div class="row align-items-center">
+                                    <!-- image poster sign up -->
+                                    <div class="col-12 col-lg-6 d-none d-lg-block border-end">
+                                        <div>
+                                            <img src="assets/images/image-signUp.svg" alt="gambar-logo-signUp" class="img-fluid">
                                         </div>
                                     </div>
+                                    <!-- form sign up -->
+                                    <div class="col-12 col-lg-6 p-3">
+                                        <form method="post">
+                                            <h3 class="mb-4 fs-1">Sign up</h3>
 
-                                    <div class="mb-4">
-                                        <?php $hasErrorPassword = isset($resultValidation["password"]) ?>
-                                        <p class="text-danger"><i><?= $hasErrorPassword ? $resultValidation["password"] : null  ?></i></p>
-                                        <div class="col-sm-10 w-100">
-                                            <input class="form-control <?= $hasErrorPassword ? 'is-invalid' : null ?>" type="password" name="password" placeholder="password" />
-                                        </div>
-                                    </div>
+                                            <?= (isset($resultRegisterUser) && $resultRegisterUser == 1) ? Alert("akun berhasil didaftarkan", true) : null ?>
 
-                                    <div class="mt-5">
-                                        <div class="col-auto">
-                                            <button type="submit" name="tombol-daftar" class="btn btn-success w-100">Daftar</button>
-                                        </div>
-                                        <div class="col-auto mt-4">
-                                            <p class="text-lead text-center">
-                                                Atau sudah ada akun, silakan <a class="text-link" href="login.php">Login</a>
-                                            </p>
-                                        </div>
+                                            <?= (isset($messagesErrors)) ? Alert($messagesErrors, false) : null ?>
+
+                                            <div class="mb-4">
+                                                <?php $hasErrorUsername = isset($resultValidation["username"]) ?>
+                                                <p class="text-danger"><i><?= $hasErrorUsername ? $resultValidation["username"] : null  ?></i></p>
+                                                <div class="input-group">
+                                                    <span class="input-group-text rounded-start" id="icon-at">@</span>
+                                                    <div class="form-floating">
+                                                        <input type="text" class="form-control <?= $hasErrorUsername ? 'is-invalid' : null ?>" name="username" placeholder="username" id="usernameInput" />
+                                                        <label for="usernameInput" class="text-dark-emphasis">username</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="mb-4">
+                                                <?php $hasErrorPassword = isset($resultValidation["password"]) ?>
+                                                <p class="text-danger"><i><?= $hasErrorPassword ? $resultValidation["password"] : null  ?></i></p>
+                                                <div class="input-group">
+                                                    <span class="input-group-text rounded-start" id="icon-hash">🔒</span>
+                                                    <div class="form-floating">
+                                                        <input class="form-control <?= $hasErrorPassword ? 'is-invalid' : null ?>" type="password" name="password" placeholder="passwordInput" />
+                                                        <label for="passwordInput" class="text-dark-emphasis">password</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="mt-5">
+                                                <div class="col-auto">
+                                                    <button type="submit" name="tombol-daftar" class="btn btn-info p-2 w-100">
+                                                        Daftar
+                                                    </button>
+                                                </div>
+                                                <div class="col-auto mt-4">
+                                                    <p class="text-lead text-center">
+                                                        Atau sudah ada akun, silakan <a class="text-link" href="login.php">Login</a>
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </form>
                                     </div>
-                                </form>
+                                </div>
                             </div>
                         </div>
                     </div>
